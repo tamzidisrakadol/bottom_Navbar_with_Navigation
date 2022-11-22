4998131b714a76911d6012306186eead2779c4d9
Tamzid Israk <tamzidisrak12@gmail.com>
2022-11-23 02:02:34 +0600

    chip bottom nav bar with Navigation Component chipnavigationbar doesnt support with Navigation Component navigation Component only support with android default android Bottom NavigationBar

--- app/src/main/java/com/example/bottomnavbarwithnavgation/MainActivity.java
+++ app/src/main/java/com/example/bottomnavbarwithnavgation/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.bottomnavbarwithnavgation;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.bottomnavbarwithnavgation.fragment.Dashboard;
+import com.example.bottomnavbarwithnavgation.fragment.Manage;
+import com.example.bottomnavbarwithnavgation.fragment.Profile;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
 public class MainActivity extends AppCompatActivity {
+    ChipNavigationBar chipNavigationBar;
+    NavController navController;
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        chipNavigationBar = findViewById(R.id.btmnavBar);
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i){
+                    case R.id.dashboard:
+                        fragment = new Dashboard();
+                        break;
+                    case R.id.manage:
+                        fragment = new Manage();
+                        break;
+                    case R.id.profile:
+                        fragment = new Profile();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,fragment).commit();
+            }
+        });
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
